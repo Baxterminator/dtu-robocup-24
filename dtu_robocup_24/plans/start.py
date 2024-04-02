eb8bf949c9c1f7c0899e061d95b9204facb62d87
Zjllll00 <115778363+Zjllll00@users.noreply.github.com>
2024-04-02 17:19:25 +0200

    start.py

--- dtu_robocup_24/plans/start.py
+++ dtu_robocup_24/plans/start.py
@@ -33,13 +33,37 @@ class StartTask(BaseTask):
 
     def loop(self) -> None:
         match self.state:
-            case TaskStep.START:
-                self.logger.info("start ...")
-                self.control.follow_line(True, 0.03, 0.4)
+            case TaskStep.START_FORWARD:
+                self.logger.info("start and go forward 74.5cm...")
+                self.data.reset_distance()
+                self.control.set_vel_h(0.1,0.0)
 
-                if self.data.distance >= 8:
+                if self.data.distance > 0.745
+                    self.control.set_vel_h(0.0,0.0)
+                    self.data.reset_distance()
+                    self.state = TaskStep.RIGHT_90_1
+                
+            case TaskStep.START_TURN_RIGHT:
+                self.logger.info("Turn right 90 degree...")
+                # adjust pose
+                self.data.reset_time()
+                self.control.set_vel_h(0.0,-0.1)
+                
+                if self.data.time_elapsed >= (math.pi/0.2):
+                    self.control.set_vel_h(0,0)
+                    self.data.reset_time()
+                    self.state = TaskStep.START_GO_TO_RAMP
+
+            case TaskStep.START_GO_TO_RAMP
+                self.logger.info("Move forward 629cm...")
+                self.data.reset_distance()
+                self.control.set_vel_h(0.1,0.0)
+
+                if self.data.distance > 6.29
+                    self.control.set_vel_h(0.0,0.0)
+                    self.data.reset_distance()
                     self.state = TaskStep.DONE
-
+        
             case TaskStep.DONE:
                 self.control.set_vel_w(0, 0)
                 self.stop = True
